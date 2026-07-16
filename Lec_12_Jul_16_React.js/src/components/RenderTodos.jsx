02e184686eb6ba28afd2c3be255e45dc2c3da595
MohdShavezz <mohdpctebtech19cse@gmail.com>
2026-07-16 15:25:32 +0530

    Searching functionality impliment

--- Lec_12_Jul_16_React.js/src/components/RenderTodos.jsx
+++ Lec_12_Jul_16_React.js/src/components/RenderTodos.jsx
@@ -1,34 +1,51 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
-const RenderTodos = ({todos,isPrivate,handleUpdate,handleDelete}) => {
+const RenderTodos = ({ todos, isPrivate, handleUpdate, handleDelete }) => {
+
+    const [searchTerm, setSarchTerm] = useState('')
+   
+    const filterTodos=useMemo(()=>{
+        return todos.filter((todo)=>{
+            const search=searchTerm.toLocaleLowerCase() //
+            return(
+                todo.title.toLocaleLowerCase().includes(search)||
+                todo.description.toLocaleLowerCase().includes(search)
+            )
+        })
+    },[todos,searchTerm])
+
+
     return (
         <div className="todo-list">
-            <h1>Todo List</h1>
+            <div style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+                <h1>Todo List</h1>
+                <input value={searchTerm} style={{border:'1px solid' , borderRadius:'.5rem',padding:'.3rem 2rem'}} type="searchTerm" placeholder='search..' onChange={e => setSarchTerm(e.target.value)} />
+            </div>
 
-            {todos.length > 0 ? (
-                todos.map((todo) => (
+            {filterTodos.length > 0 ? (
+                filterTodos.map((todo) => (
                     <div className="todo-item" key={todo.id}>
                         <h3>{todo.title}</h3>
                         <p>{todo.description}</p>
-                    {
-                        !isPrivate&&
-                         <div className="todo-actions">
-                            <button
-                                className="update-btn"
-                                onClick={() => handleUpdate(todo.id)}
-                            >
-                                Update
-                            </button>
+                        {
+                            !isPrivate &&
+                            <div className="todo-actions">
+                                <button
+                                    className="update-btn"
+                                    onClick={() => handleUpdate(todo.id)}
+                                >
+                                    Update
+                                </button>
 
-                            <button
-                                className="delete-btn"
-                                onClick={() => handleDelete(todo.id)}
-                            >
-                                Delete
-                            </button>
-                        </div>
-                    }
-                       
+                                <button
+                                    className="delete-btn"
+                                    onClick={() => handleDelete(todo.id)}
+                                >
+                                    Delete
+                                </button>
+                            </div>
+                        }
+
                     </div>
                 ))
             ) : (
